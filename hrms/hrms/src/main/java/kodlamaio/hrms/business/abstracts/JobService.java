e3800bd67c360cc84e7a34a911c889f3de7cfa03
Mustafa Özkarabulut <81626732+MustafaOzkarabulut@users.noreply.github.com>
2021-05-29 18:24:14 +0300

    Delete hrms directory

--- hrms/hrms/src/main/java/kodlamaio/hrms/business/abstracts/JobService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.business.abstracts;
-
-import java.util.List;
-
-import kodlamaio.hrms.entities.concretes.job;
-
-public interface JobService {
-	List<job> getAll();
-}
--- hrms/hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.hrms.entities.concretes.job;
-
-public interface JobDao extends JpaRepository<job,Integer>{
-
-}
